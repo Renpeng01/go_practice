e6a5d3cf20cacb13365e0a73cfdefe80c0b8424f
renpeng <renpeng@hellogroup.com>
2025-03-20 09:42:46 +0800

    add

--- src/slice/main.go
+++ src/slice/main.go
@@ -39,14 +39,56 @@ func SliceCopy() {
 	fmt.Printf("change a: %+v, b: %+v\n", a[0].Val, b[0].Val)
 }
 
-func main() {
-	// SliceCopy()
+// func main() {
+// 	// SliceCopy()
 
-	arr := [3]int{1, 2, 3}
-	slice := arr[:]
-	// 使用数组初始化另一个数组后，通过index改变元素，会源数组相同位置的元素
-	// 使用下标初始化切片不会拷贝原数组或者原切片中的数据，只会创建一个指向原数组的切片结构体，修改新切片的数据也会修改原切片
-	slice[0] = 0
-	fmt.Println("arr: ", arr)
-	fmt.Println("slice: ", slice)
+// 	arr := [3]int{1, 2, 3}
+// 	slice := arr[:]
+// 	// 使用数组初始化另一个数组后，通过index改变元素，会源数组相同位置的元素
+// 	// 使用下标初始化切片不会拷贝原数组或者原切片中的数据，只会创建一个指向原数组的切片结构体，修改新切片的数据也会修改原切片
+// 	slice[0] = 0
+// 	fmt.Println("arr: ", arr)
+// 	fmt.Println("slice: ", slice)
+// }
+
+// func main() {
+// 	a := []int{1, 2, 3, 4}
+// 	s1 := a[0:2]
+
+// 	fmt.Printf("s1 len: %+v, cap: %+v, s1: %+v\n", len(s1), cap(s1), s1)
+
+// 	s1 = append(s1, 0)
+
+// 	fmt.Printf("after s1 append a: %+v", a)
+
+// }
+
+func insert(slice []int, index int, value int) []int {
+	// 在指定位置插入元素
+	slice = append(slice[:index], append([]int{value}, slice[index:]...)...)
+	return slice
+}
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5}
+
+	v := 99 // 待插入元素
+
+	a := slice[:2]
+	b := slice[2:]
+
+	// 正确方式
+	t := append([]int{v}, b...)
+	a = append(a, t...)
+
+	// 错误方式
+	// a = append(a, v)
+	// a = append(a, b...)
+
+	fmt.Println(a)
+
+	// // 在索引 2 的位置插入元素 99
+	// slice = insert(slice, 2, 99)
+
+	// fmt.Println(slice) // 输出: [1 2 99 3 4 5]
 }
