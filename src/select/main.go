1e33fb31a8fecc568ef28d03446e5cb62a509edb
renpeng <renpeng@hellogroup.com>
2025-03-28 11:29:36 +0800

    add

--- src/select/main.go
+++ src/select/main.go
@@ -1,25 +1,40 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+// func main() {
+
+// 	go func() {
+// 		for {
+// 			fmt.Println(time.Now())
+// 			time.Sleep(3 * time.Second)
+// 		}
+// 	}()
+// 	// var ch chan int
+
+// 	// ch := make(chan int)
+// 	// select {
+// 	// case ch <- 1:
+// 	// 	fmt.Println("<-ch")
+// 	// default:
+// 	// 	fmt.Println("exec default")
+// 	// }
+
+// 	select {}
+
+// }
+
+func Expr(n int) int {
+	fmt.Println(n)
+	return n
+}
 
 func main() {
-
-	go func() {
-		for {
-			fmt.Println(time.Now())
-			time.Sleep(3 * time.Second)
-		}
-	}()
-	// var ch chan int
-
-	ch := make(chan int, 10)
-	select {
-	case ch <- 1:
-		fmt.Println("<-ch")
-		// default:
-		// 	fmt.Println("exec default")
+	switch Expr(2) {
+	case Expr(1), Expr(2), Expr(3):
+		fmt.Println("case 1")
+		fallthrough // 强行执行下一个代码块且不进行条件判断
+	case Expr(4):
+		fmt.Println("case 2")
 	}
 }
